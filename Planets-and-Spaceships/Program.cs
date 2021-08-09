b62a05557f9052992b270ed0dcdcdd559908e685
Martin Sisk <Brothermj@hotmail.com>
2021-08-09 17:49:31 -0500

    initial commit to repo.

---
+++ Planets-and-Spaceships/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Planets_and_Spaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Planets & Spaceships\n\n");
+
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            Console.WriteLine($"Planets in the Solar System: {string.Join(", ", planetList)}.");
+        }
+    }
+}
